946b006c75f85e8140b946a6d30a6c4669315bcb
Kacper Lechicki <klechicki@poczta.fm>
2022-12-22 16:37:25 +0100

    ready

--- js/script.js
+++ js/script.js
@@ -4,29 +4,40 @@ const cards = document.querySelectorAll('.card');
 
 let cardOne, cardTwo;
 let disabled = false;
+let matchedCards = 0;
 
 const flipCard = (e) => {
 	let clickedCard = e.target;
-	clickedCard.classList.add('flip');
+	if (clickedCard !== cardOne && !disabled) {
+		clickedCard.classList.add('flip');
 
-	if (!cardOne && !disabled) {
-		return (cardOne = clickedCard);
+		if (!cardOne) {
+			return (cardOne = clickedCard);
+		}
+		cardTwo = clickedCard;
+		disabled = true;
+
+		let cardOneImg = cardOne.querySelector('img').src,
+			cardTwoImg = cardTwo.querySelector('img').src;
+
+		matchingCards(cardOneImg, cardTwoImg);
 	}
-	cardTwo = clickedCard;
-    disabled = true;
-
-	let cardOneImg = cardOne.querySelector('img').src,
-		cardTwoImg = cardTwo.querySelector('img').src;
-
-	matchingCards(cardOneImg, cardTwoImg);
 };
 
 const matchingCards = (cardOneImg, cardTwoImg) => {
 	if (cardOneImg === cardTwoImg) {
+		matchedCards++;
+
+		if (matchedCards == 8) {
+			setTimeout(() => {
+				return shuffleCard();
+			}, 1000);
+		}
+
 		cardOne.removeEventListener('click', flipCard);
 		cardTwo.removeEventListener('click', flipCard);
 		cardOne = cardTwo = '';
-		return disabled = true;
+		return (disabled = false);
 	}
 
 	setTimeout(() => {
@@ -35,12 +46,30 @@ const matchingCards = (cardOneImg, cardTwoImg) => {
 	}, 400);
 
 	setTimeout(() => {
-		cardOne.classList.remove('flip');
-		cardTwo.classList.remove('flip');
+		cardOne.classList.remove('flip', 'shake');
+		cardTwo.classList.remove('flip', 'shake');
 		cardOne = cardTwo = '';
-        disabled = false;
+		disabled = false;
 	}, 1000);
 };
+
+const shuffleCard = () => {
+	matchedCards = 0;
+	cardOne = cardTwo = '';
+	disabled = false;
+
+	let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+	arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+	cards.forEach((card, index) => {
+		card.classList.remove('flip');
+		let imgTag = card.querySelector('img');
+		imgTag.src = `./img/diamond${arr[index]}.png`;
+		card.addEventListener('click', flipCard);
+	});
+};
+
+shuffleCard();
 
 cards.forEach((card) => {
 	card.addEventListener('click', flipCard);
