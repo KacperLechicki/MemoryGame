67ad14e248e716075b44f935cf6b2d83b09ac28d
Kacper Lechicki <klechicki@poczta.fm>
2022-12-22 15:54:46 +0100

    checking if cards matched

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const cards = document.querySelectorAll('.card');
+
+let cardOne, cardTwo;
+
+const flipCard = (e) => {
+	let clickedCard = e.target;
+	clickedCard.classList.add('flip');
+
+	if (!cardOne) {
+		return (cardOne = clickedCard);
+	}
+	cardTwo = clickedCard;
+
+	let cardOneImg = cardOne.querySelector('img').src,
+		cardTwoImg = cardTwo.querySelector('img').src;
+
+	matchingCards(cardOneImg, cardTwoImg);
+};
+
+const matchingCards = (cardOneImg, cardTwoImg) => {
+	if (cardOneImg === cardTwoImg) {
+		return console.log('ok');
+	}
+	return console.log('nie ok');
+};
+
+cards.forEach((card) => {
+	card.addEventListener('click', flipCard);
+});
